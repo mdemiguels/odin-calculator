1f80864c5294d67d971f151d44ee4fc831eda7f9
mdemiguels <miguelschz1997@gmail.com>
2024-09-30 21:05:40 +0200

    add section header and styles to it

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-let number1 = '';
-let number2 = '';
-let operator = '';
+let number1 = "";
+let number2 = "";
+let operator = "";
+let displayValue = "0";
 
-const add = function(a, b) {
+const buttons = document.querySelector(".buttons");
+
+document.addEventListener("DOMContentLoaded", () => {
+  
+});
+
+const add = function (a, b) {
     a = parseInt(a);
     b = parseInt(b);
-	return a + b;
+    return a + b;
 };
 
-const subtract = function(a, b) {
-	return a - b;
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const multiply = function(a, b) {
-
-  return a * b;
-
+const multiply = function (a, b) {
+    return a * b;
 };
 
-const divide = function(a, b) {
-
-  return a / b;
-
+const divide = function (a, b) {
+    return a / b;
 };
 
-const operate = function(opeartor, a, b) {
-
+const operate = function (opeartor, a, b) {
     add(a, b);
-    
 };
