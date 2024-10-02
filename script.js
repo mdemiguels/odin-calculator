73e8adb0f3c38b5f750eb3b3c9dadd462093153c
mdemiguels <miguelschz1997@gmail.com>
2024-10-02 14:14:46 +0200

    fix bug that overflow the with of the calculator when the number is long

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ let displayValue = "0";
 
 const resultDisplay = document.querySelector(".result>span");
 const operationDisplay = document.querySelector(".operation>span");
-const buttons = document.querySelector(".buttons");
+const numberButtons = document.querySelectorAll(".number-row button");
 
 document.addEventListener("DOMContentLoaded", () => {
-  resultDisplay.textContent = displayValue;
+    resultDisplay.textContent = displayValue;
+
+    numberButtons.forEach((button) => {
+        button.addEventListener("click", (evt) => {
+            displayValue = button.firstChild.textContent;
+            refreshDisplay();
+        });
+    });
 });
 
 const add = function (a, b) {
@@ -31,4 +38,12 @@ const divide = function (a, b) {
 
 const operate = function (opeartor, a, b) {
     add(a, b);
+};
+
+const refreshDisplay = function () {
+  if (resultDisplay.textContent === "0") {
+    resultDisplay.textContent = displayValue;
+  } else {
+    resultDisplay.textContent += displayValue;
+  }
 };
